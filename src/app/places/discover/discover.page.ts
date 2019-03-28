7416f8b80a60aadaa5a136c853cdb04bb067e4a7
ahmed maamoun <ahmedfcisasu@gmail.com>
2019-03-29 00:12:17 +0200

    updating

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { PlacesService } from '../places.service';
-import { Place } from '../place.model';
+import { Component, OnInit } from "@angular/core";
+import { PlacesService } from "../places.service";
+import { Place } from "../place.model";
+import { MenuController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-discover',
-  templateUrl: './discover.page.html',
-  styleUrls: ['./discover.page.scss']
+  selector: "app-discover",
+  templateUrl: "./discover.page.html",
+  styleUrls: ["./discover.page.scss"]
 })
 export class DiscoverPage implements OnInit {
   places: Place[];
-  constructor(private placesService: PlacesService) {}
+  constructor(
+    private placesService: PlacesService,
+    private menuCtrl: MenuController
+  ) {}
 
   ngOnInit() {
     this.places = this.placesService.places;
+  }
+  openMenu(){
+    this.menuCtrl.toggle();
   }
 }
